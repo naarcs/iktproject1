72bd4bed023924a619446028e8e95236941ccc80
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-10-26 18:27:57 +0200

    Összehasonlitó Operátorok

--- pyton/01/operatorok.py
+++ pyton/01/operatorok.py
@@ -26,3 +26,40 @@ print(eredmeny1)
 
 eredmeny2=10/3
 print(eredmeny2)
+eredmeny2=10//3
+print(eredmeny2)
+eredmeny2=10%3
+print(eredmeny2)
+
+eredmeny3=2**8
+print(eredmeny3)
+
+szovegesEredmeny=szoveg1+szoveg2
+print(szovegesEredmeny)
+szovegesEredmeny2=szoveg1*5
+print(szovegesEredmeny2)
+
+
+#Összehasonlito Operátorok - "<, >, <=, >=, !=, =="
+#Egyszerű egyirányú elágazás
+
+if (valosSzam1<1000):
+    print("A valosSzam1 értéke kisebb mint 1000.")
+    print("Vizsgálat vége.")
+
+#Összetett két irányu elágazás
+
+if (egeszSzam1>500):
+    print("Az egeszSzam1 változó értéke nagyobb mint 500.")
+else:
+    print("Az egeszSzam1 vátozó értéke kisebb vagy egyenlő mint 500.")
+
+#Összetett több irányú elágazás
+
+if (egeszSzam1==245):
+    print("Az egeszSzam1 értéke 245.")
+elif (egeszSzam1>245):
+    print("Az egeszSzam1 értéke nagyobb mint 245.")
+else:
+    print("Az egeszSzam1 értéke kisebb mint 245.")        
+
