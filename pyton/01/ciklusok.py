741368fe2c1631297735746dc7efe1c226ef6056
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-10-26 19:17:09 +0200

    Ciklusok

---
+++ pyton/01/ciklusok.py
@@ -0,0 +1,29 @@
+
+
+#Számláló ciklus (for)
+
+for i in range(10):
+    print(i)
+
+for i in range(2,15):
+    print(i)    
+
+for i in range(4,10,2):
+    print(i)
+
+for i in range(100,10,-5):
+    print(i)
+    continue
+    print("Valami")
+
+for i in range(10,-2,1):       #nem lehet
+    print("asdsdga")
+
+
+
+
+
+#Elöltesztelő ciklus
+
+while (not True):
+    print("Forever")
