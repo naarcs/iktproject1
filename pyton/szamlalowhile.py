97c21563a42c6f44af7a7fea16f6f71a9745a395
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-02 16:52:10 +0100

    While

---
+++ pyton/szamlalowhile.py
@@ -0,0 +1,20 @@
+
+
+# While ciklus(szamlalo)
+
+i = 0 
+while(i<3):
+    print(i)
+    i += 1
+
+
+
+
+
+#for e in range(3,0,-1):
+#    print(e)    
+
+e = 3 
+while(e>0):
+    print(e)
+    e -= 1
