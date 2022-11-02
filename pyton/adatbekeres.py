7dcaa826aa91d67ea17c4bf7734211d9131ff880
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-02 16:41:44 +0100

    Adatbekérés

---
+++ pyton/adatbekeres.py
@@ -0,0 +1,14 @@
+
+
+# Szöveges adat bekérése a konzolrol(billentyü)
+
+
+
+szam1 = int(input("Kérem az első számot: "))  #egesz szám 
+#print(szam1)
+
+szam2 = int(input("Második szám: "))
+print(szam2 + szam1)
+
+valosSzam1 = float(input("Valós szám: "))  #valos szam 
+print(valosSzam1)
