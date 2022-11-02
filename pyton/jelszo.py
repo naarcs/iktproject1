f95b77db471ffa3682a4e4cc3dcdd020697d3a01
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-02 17:21:01 +0100

    Python orai munka

--- pyton/jelszo.py
+++ pyton/jelszo.py
@@ -1,5 +1,23 @@
 
 
 
+from time import sleep
+
+
 jelszo = "Kando"
 
+
+marad = 3 
+while(marad > 0):
+    bekertjelszo = input("Kérem a jelszót: ")
+    if (bekertjelszo == jelszo):
+        print("Sikeres bejelentkezés")
+        marad = 0 
+    if (marad == 1):
+        print("10perc várakozás...")
+        sleep(10)
+        marad = 4
+    marad -= 1
+print("Dolgozhat!")
+
+    
