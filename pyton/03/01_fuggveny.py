563026318b2adf1944878d6edbea139b1895066a
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-09 16:57:55 +0100

    01_függvény

---
+++ pyton/03/01_fuggveny.py
@@ -0,0 +1,20 @@
+
+import random
+
+def OsszeHasonlitas(rSzam,bSzam):
+    if (bSzam < rSzam):
+        return f"A gép nagyobb számra gondolt: {rSzam}"
+    elif (bSzam > rSzam):
+        return f"A gép kisebb számra gondolt: {rSzam}"
+    else:
+        return f"Eltaláltad a gép által gondolt számot! {rSzam}"
+
+
+rSzam = random.randrange(1,17)
+bSzam = int(input("Adj meg egy számot!"))
+
+print(OsszeHasonlitas(rSzam,bSzam))
+
+
+
+
