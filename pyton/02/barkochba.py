58b1fe30bf30c6584223fe403e0d816aedf9bb35
Naár Csaba <NaarCs@kkszki.local>
2022-11-09 16:17:46 +0100

    Egyszerűsített vezrio

--- pyton/02/barkochba.py
+++ pyton/02/barkochba.py
@@ -4,10 +4,10 @@ import random
 
 rSzam = random.randrange(1,17)
 
-bekertSzam = int(input("Adj meg egy számot!"))
-if (bekertSzam < rSzam):
+bSzam = int(input("Adj meg egy számot!"))
+if (bSzam < rSzam):
     print(f"A gép nagyobb számra gondolt: {rSzam}")
-elif (bekertSzam > rSzam):
+elif (bSzam > rSzam):
     print(f"A gép kisebb számra gondolt: {rSzam}")
 else:
     print(f"Eltaláltad a gép által gondolt számot! {rSzam}")
