95e3d15d1549b4b0c8ed9b168e0ed680d08bf06c
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-09 18:10:50 +0100

    jelszo

---
+++ pyton/03/jelszo.py
@@ -0,0 +1,15 @@
+
+
+def bejelentkezes(nev, jelszo):
+    if (nev == "bori99" and jelszo == "Szivecske<3"):
+        return True
+    return False
+
+
+sikeresBelepes = False
+while (not sikeresBelepes):
+    nev = input("Adja meg a felhasználó nevét! ")
+    jelszo = input("Adja meg a jelszavát! ")
+    sikeresBelepes = bejelentkezes(nev,jelszo)
+print("Sikeres bejelentkezés")
+
