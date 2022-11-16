782c399bb587b5257a8e66dcbbdb428a6e62b9a8
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-16 17:19:32 +0100

    tombok

--- pyton/04/02-tomb.py
+++ pyton/04/02-tomb.py
@@ -4,5 +4,24 @@ kutya = []
 print("Add mg 5 kutya nevét: ")
 for i in range(1,6):
     kutya.append(input(f"{i} kutya neve: "))
-
 print(kutya)
+
+
+for egykutya in kutya:               #igy tudunk egy tombe keresni
+    if (len(egykutya) == 2):
+        print(egykutya)                
+
+
+
+
+
+
+"""
+for i in range(len(kutya)):          #ez is az 
+    print(kutya[i])
+
+i = 0                                 
+while (i<len(kutya)):                  #ez is ugyan azt csinalja 
+    print(kutya[i])
+    i+=1
+"""
