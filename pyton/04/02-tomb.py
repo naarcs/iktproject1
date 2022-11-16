1fede825b11081d89d1f5188b26f40ae8c81eaca
Naár Csaba <NaarCs@KKSZKI.LOCAL>
2022-11-16 17:04:07 +0100

    tombok

---
+++ pyton/04/02-tomb.py
@@ -0,0 +1,8 @@
+
+kutya = []
+
+print("Add mg 5 kutya nevét: ")
+for i in range(1,6):
+    kutya.append(input(f"{i} kutya neve: "))
+
+print(kutya)
---
+++ pyton/04/12-tomb.py
@@ -0,0 +1,17 @@
+
+
+szo1 = "alma"
+szo2 = "kotre"
+szo3 = "szilva"
+szo4 = "barack"
+szo5 = "banán"
+
+zoldsegek = []
+gyomolcsok = ["alma", "korte", "szilva", "barack", "banán"]
+print(gyomolcsok)
+print(gyomolcsok[4])
+print(zoldsegek)
+zoldsegek.append("paradicson")
+zoldsegek.append("paprika")
+print(zoldsegek)
+
